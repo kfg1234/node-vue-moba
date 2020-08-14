d88bc845e7dd25efe043da8253bb0f270ca70264
kfg <1125303286@qq.com>
2020-08-14 18:21:18 +0800

    2020年8月14日18:21:17

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>创建分类</h1>
+        <el-form label-width="80px">
+            <el-form-item label="名称">
+                <el-input v-model="model.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            model: {},
+        };
+    },
+};
+</script>
